15ecffb0365a6318aca7cd7083ab2e2ea7ebfb85
Thailan Godoy <thailanrg@gmail.com>
2024-07-18 22:47:22 -0300

    refactoring

---
+++ app/src/main/java/com/example/projetoestudomatheus/help/extension.kt
@@ -0,0 +1,8 @@
+package com.example.projetoestudomatheus.help
+
+import com.google.android.material.textfield.TextInputEditText
+
+
+fun TextInputEditText.value():String{
+    return this.toString()
+}
--- app/src/main/java/com/example/projetoestudomatheus/presentation/FormularyActivity.kt
+++ app/src/main/java/com/example/projetoestudomatheus/presentation/FormularyActivity.kt
@@ -2,11 +2,11 @@ package com.example.projetoestudomatheus.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projetoestudomatheus.data.DataBase
 import com.example.projetoestudomatheus.data.Noticias
 import com.example.projetoestudomatheus.databinding.ActivityFormularyBinding
+import com.example.projetoestudomatheus.help.value
 
 class FormularyActivity : AppCompatActivity() {
 
@@ -18,18 +18,23 @@ class FormularyActivity : AppCompatActivity() {
         setupForm()
     }
 
-    private fun setupForm() = with(binding){
+    private fun setupForm() = with(binding) {
         cadastrar.setOnClickListener {
-            val newNotice = Noticias(
-                title.text.toString(),
-                descricao.text.toString(),
-                mensagem.text.toString()
+            DataBase().setItem(
+                Noticias(
+                    titlEdt.value(),
+                    descricaoEdt.value(),
+                    mensagemEdt.value()
+                )
             )
-            DataBase().setItem(newNotice)
-            val intent = Intent(this@FormularyActivity, MainActivity::class.java)
-            startActivity(intent)
+            Intent().apply {
+                startActivity(
+                    Intent(
+                        this@FormularyActivity,
+                        MainActivity::class.java
+                    )
+                )
+            }
         }
-
     }
-
 }
